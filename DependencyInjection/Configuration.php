20a60157548d3942e6ccb85f5f73ebe527866c40
Hetru Mathieu <mathieu.hetru@univ-lille.fr>
2023-12-22 19:24:20 +0100

    modif for sf7.0

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -15,7 +15,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritdoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('l3_cas_guard');
 
