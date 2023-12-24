5952cba16dfb4c6fcf7958727aa310868a3378d6
Hetru Mathieu <mathieu.hetru@univ-lille.fr>
2023-12-24 15:05:52 +0100

    modif for sf7.0

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -26,7 +26,7 @@ class Configuration implements ConfigurationInterface
             ->scalarNode('port')->defaultValue(443)->end()
             ->scalarNode('ca')->defaultNull()->end()
             ->booleanNode('handleLogoutRequest')->defaultValue(false)->end()
-            ->scalarNode('casLoginTarget')->defaultNull()->end()
+            ->scalarNode('casServiceBaseUrl')->defaultNull()->end()
             ->scalarNode('casLogoutTarget')->defaultNull()->end()
             ->booleanNode('force')->defaultValue(true)->end()
             ->booleanNode('gateway')->defaultValue(true)->end()
